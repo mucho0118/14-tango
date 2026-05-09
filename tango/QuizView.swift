c9b39c5a9946898d1865b08e927ac158911db8b0
mucho0118 <164824393+mucho0118@users.noreply.github.com>
2026-05-09 16:40:11 +0900

    Upload quiz view

---
+++ tango/QuizView.swift
@@ -0,0 +1,81 @@
+//
+//  QuizView.swift
+//  tango
+//
+//  Created by 長大夢 on 2026/05/06.
+//
+import SwiftUI
+import SwiftData
+
+struct QuizView: View{
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var context
+    @Query var words: [Word]
+    @State var index = 0
+    @State var showAnswer = false
+    
+    var isLastQuiz: Bool {
+        index + 1 >= words.count
+    }
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            Spacer()
+            
+            
+            if words.isEmpty {
+                Text("登録されている単語がありません")
+            } else {
+                Text(words[index].english)
+                    .font(.largeTitle)
+                
+                Text(showAnswer ? words[index].japanese : "-")
+                    .font(.largeTitle)
+                
+                Button {
+                    showAnswer.toggle()
+                }label : {
+                    Text(showAnswer ? "答えを非表示" : "答えを表示")
+                }
+                .font(.title)
+                .foregroundStyle(.white)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity)
+                .background(Color.orange)
+                .clipShape(.capsule)
+                
+                
+                
+            }
+            Spacer()
+            
+            Button {
+                if isLastQuiz{
+                    
+                    dismiss()
+                    
+                } else{
+                    index += 1
+                    showAnswer = false
+                    
+                }
+            } label: {
+                Text(isLastQuiz ? "終了" : "次の問題へ")
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding(.vertical, 12)
+            .frame(maxWidth: .infinity)
+            .background(Color.orange)
+            .clipShape(.capsule)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 60)
+    }
+}
+
+#Preview{
+    QuizView()
+        .modelContainer(wordContainer)
+}
